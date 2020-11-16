a1cf1e23c7d15efb06610c1c664df98f4a0c507e
sksaifuddin <atushaik@gmail.com>
2020-11-16 16:45:20 +0530

    main branch ready

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 new Vue({
     el: '#vue-app',
     data: {
-        health: 100,
-        ended: false
+       
     },
     methods: {
-      punch: function() {
-          this.health -= 10;
-          if(this.health <= 0) {
-              this.ended = true;
-          }
-      },
-      restart: function() {
-          this.health = 100;
-          this.ended = false
-      }
+     
     }
 })
